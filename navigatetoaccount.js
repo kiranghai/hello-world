dcb99400d05f916e949559bffb7f59e343cbea7e
kiranghai <81906341+kiranghai@users.noreply.github.com>
2026-05-15 12:30:33 +0530

    Create navigatetoaccount.js

---
+++ navigatetoaccount.js
@@ -0,0 +1,24 @@
+{
+  "Type": "Navigate Action",
+  "propertySetConfig": {
+    "controlWidth": 12,
+    "label": "NavigateToAccount",
+    "validationRequired": "Submit",
+    "targetType": "Record",
+    "objectAction": "home",
+    "recordAction": "view",
+    "loginAction": "login",
+    "targetFilter": "Recent",
+    "targetId": "%Account_1:Id%",
+    "targetName": "Account",
+    "targetLWCLayout": "lightning",
+    "variant": "brand",
+    "iconName": "",
+    "iconVariant": "",
+    "iconPosition": "left",
+    "replace": true,
+    "pubsub": false,
+    "message": {},
+    "HTMLTemplateId": ""
+  }
+}
